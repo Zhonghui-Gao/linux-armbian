be3bca6533e58bf24de6e7e1a2ed85648f58aa5e
Zhonghui-Gao <914161777@qq.com>
2023-08-16 10:37:28 +0800

    hm3568

--- 6.4__rockchip64__arm64/include/generated/compile.h
+++
@@ -1,4 +0,0 @@
-#define UTS_MACHINE		"aarch64"
-#define LINUX_COMPILE_BY	"gao"
-#define LINUX_COMPILE_HOST	"NULL"
-#define LINUX_COMPILER		"aarch64-linux-gnu-gcc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0, GNU ld (GNU Binutils for Ubuntu) 2.38"
--- 6.4__rockchip64__arm64/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "6.4.8"
